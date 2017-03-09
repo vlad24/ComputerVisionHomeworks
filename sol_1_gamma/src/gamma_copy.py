c66331efd7db9b5685bd7c72d48aece3a020c0e2
Vladislav Pavlov <vlad.pavlov24@gmail.com>
2017-03-09 20:12:03 +0300

    v2. Negatibe added

--- sol_1_gamma/src/gamma_copy.py
+++ sol_1_gamma/src/gamma_copy.py
@@ -16,12 +16,23 @@ def log_correction(img):
             b[i][j] = np.log(1.0 + img[i][j] / 255.0)
     return np.uint8(b * 255)
 
-alpha = 0.5
+def neg_correction(img):
+    print "Negative"
+    b = np.zeros(img.shape)
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            b[i][j] = 1.0 - img[i][j] / 255.0
+    return np.uint8(b * 255)
+
 image = cv2.imread('kitty.jpg', cv2.IMREAD_GRAYSCALE)
+alpha = 0.5
 gamma_image = gamma_correction(image, alpha)
 log_image = log_correction(image)
+neg_image = neg_correction(image)
 
 cv2.imshow("Gamma {}".format(alpha), gamma_image)
 cv2.waitKey()
 cv2.imshow("Logarithm result", log_image)
+cv2.waitKey()
+cv2.imshow("Negative result", neg_image)
 cv2.waitKey()
